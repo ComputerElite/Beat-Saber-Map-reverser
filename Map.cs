976b365a4975972b100f88900b3f2b8960c49263
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2021-05-08 11:29:57 +0200

    Add files

---
+++ Map.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+namespace BS_Map_reverser
+{
+
+    public class BeatSaberSong
+    {
+        [JsonPropertyName("_songName")]
+        public string SongName { get; set; } = "N/A";
+        [JsonPropertyName("_songSubName")]
+        public string SubName { get; set; } = "N/A";
+        [JsonPropertyName("_songAuthorName")]
+        public string SongArtist { get; set; } = "N/A";
+        [JsonPropertyName("_levelAuthorName")]
+        public string Mapper { get; set; } = "N/A";
+        [JsonPropertyName("_beatsPerMinute")]
+        public float BPM { get; set; } = 0.0f;
+        public string Hash { get; set; } = "N/A";
+        public string Key { get; set; } = "N/A";
+        public string hash { get; set; } = "N/A";
+        public string key { get; set; } = "N/A";
+        public bool cached { get; set; } = false;
+        public bool RequestGood { get; set; } = false;
+        [JsonPropertyName("_difficultyBeatmapSets")]
+        public List<BeatSaberSongBeatMapCharacteristic> BeatMapCharacteristics { get; set; } = new List<BeatSaberSongBeatMapCharacteristic>();
+        public JsonElement _customData { get; set; } = JsonSerializer.Deserialize<JsonElement>("{}");
+
+        public string _allDirectionsEnvironmentName { get; set; } = "N/A";
+        public string _environmentName { get; set; } = "N/A";
+        public string _coverImageFilename { get; set; } = "N/A";
+        public string _songFilename { get; set; } = "N/A";
+        public float _previewDuration { get; set; } = 0.0f;
+        public float _previewStartTime { get; set; } = 0.0f;
+        public float _shufflePeriod { get; set; } = 0.0f;
+        public float _shuffle { get; set; } = 0.0f;
+        public float _songTimeOffset { get; set; } = 0.0f;
+        public string _version { get; set; } = "N/A";
+
+        public string GetJSON()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+
+    public class BeatSaberSongDifficulty
+    {
+        public float Duration { get; set; } = 0.0f;
+        public float Length { get; set; } = 0.0f;
+        [JsonPropertyName("_noteJumpMovementSpeed")]
+        public double NJS { get; set; } = 0.0;
+        [JsonPropertyName("_noteJumpStartBeatOffset")]
+        public float NJSOffset { get; set; } = 0.0f;
+        public int Bombs { get; set; } = 0;
+        public int Notes { get; set; } = 0;
+        public int Obstacles { get; set; } = 0;
+        [JsonPropertyName("_difficulty")]
+        public string DifficultyName { get; set; } = "N/A";
+        [JsonPropertyName("_difficultyRank")]
+        public int DifficultyID { get; set; } = 0;
+        public JsonElement _customData { get; set; } = JsonSerializer.Deserialize<JsonElement>("{}");
+
+        public string _beatmapFilename { get; set; } = "N/A";
+
+        public bool IsEasy()
+        {
+            if (DifficultyName.ToLower() == "easy") return true;
+            else return false;
+        }
+
+        public bool IsNormal()
+        {
+            if (DifficultyName.ToLower() == "normal") return true;
+            else return false;
+        }
+
+        public bool IsHard()
+        {
+            if (DifficultyName.ToLower() == "hard") return true;
+            else return false;
+        }
+
+        public bool IsExpert()
+        {
+            if (DifficultyName.ToLower() == "expert") return true;
+            else return false;
+        }
+
+        public bool IsExpertPlus()
+        {
+            if (DifficultyName.ToLower() == "expertplus") return true;
+            else return false;
+        }
+    }
+
+    public class BeatSaberSongBeatMapCharacteristic
+    {
+        [JsonPropertyName("_beatmapCharacteristicName")]
+        public string BeatMapCharacteristicName { get; set; } = "N/A";
+        [JsonPropertyName("_difficultyBeatmaps")]
+        public List<BeatSaberSongDifficulty> Difficulties { get; set; } = new List<BeatSaberSongDifficulty>();
+    }
+
+
+}
+
+namespace BeatSaber
+{
+    //read more here https://bsmg.wiki/mapping/map-format.html
+    [Serializable]
+    public class DiffFile
+    {
+        //Only 2.0.0 supported
+        public string _version { get; set; } = "2.0.0";
+        public List<Event> _events { get; set; } = new List<Event>();
+        public List<Note> _notes { get; set; } = new List<Note>();
+        public List<Obstacle> _obstacles { get; set; } = new List<Obstacle>();
+    }
+
+    public class Obstacle
+    {
+        //in beats
+        public float _time { get; set; } = 0.0f;
+        public int _lineIndex { get; set; } = 0;
+        //0, full height wall; 1, crouch/duck wall
+        public int _type { get; set; } = 0;
+        public float _duration { get; set; } = 0.0f;
+        public int _width { get; set; } = 0;
+
+    }
+
+    public class Event
+    {
+        //in beats
+        public float _time { get; set; } = 0.0f;
+        public int _lineIndex { get; set; } = 0;
+        public int _lineLayer { get; set; } = 0;
+        /*
+        0	Controls lights in the Back Lasers group.
+        1	Controls lights in the Ring Lights group.
+        2	Controls lights in the Left Rotating Lasers group.
+        3	Controls lights in the Right Rotating Lasers group.
+        4	Controls lights in the Center Lights group.
+        5	(unused) Controls boost light colors (secondary colors).
+        6	Unused.
+        7	Unused.
+        8	Creates one ring spin in the environment. Is not affected by _value.
+        9	Controls zoom for applicable rings. Is not affected by _value.
+        10	(unused) Official BPM Changes.
+        11	Unused.
+        12	Controls rotation speed for applicable lights in Left Rotating Lasers.
+        13	Controls rotation speed for applicable lights in Right Rotating Lasers.
+        14	(Previously unused) 360/90 Early rotation. Rotates future objects, while also rotating objects at the same time.
+        15	(Previously unused) 360/90 Late rotation. Rotates future objects, but ignores rotating objects at the same time.*/
+        public int _type { get; set; } = 0;
+        /*
+        ###Lights
+        0	Turns the light group off.
+        1	Changes the lights to blue, and turns the lights on.
+        2	Changes the lights to blue, and flashes brightly before returning to normal.
+        3	Changes the lights to blue, and flashes brightly before fading to black.
+        4	Unused.
+        5	Changes the lights to red, and turns the lights on.
+        6	Changes the lights to red, and flashes brightly before returning to normal.
+        7	Changes the lights to red, and flashes brightly before fading to black.*/
+
+        public int _value { get; set; } = 0;
+        //0, up; 1, down; 2, left; 3, right; 4, up left; 5, up right; 6, down left; 7 , down right; 8, any (dot note)
+        public int _cutDirection { get; set; } = 0;
+    }
+
+    public class Note
+    {
+        //in beats
+        public float _time { get; set; } = 0.0f;
+        public int _lineIndex { get; set; } = 0;
+        public int _lineLayer { get; set; } = 0;
+        //0, left red; 1, right blue; 2, unused; 3, Bomb
+        public int _type { get; set; } = 0;
+        //0, up; 1, down; 2, left; 3, right; 4, up left; 5, up right; 6, down left; 7 , down right; 8, any (dot note)
+        public int _cutDirection { get; set; } = 0;
+    }
+
+    public class BeatSaberSong
+    {
+        public string _songName;
+        public string _songSubName = "N/A";
+        public string _songAuthorName = "N/A";
+        public string _levelAuthorName = "N/A";
+        public float _beatsPerMinute;
+        public bool cached = false;
+        public bool RequestGood = false;
+        public List<BeatSaberSongBeatMapCharacteristic> _difficultyBeatmapSets = new List<BeatSaberSongBeatMapCharacteristic>();
+        //public JsonElement _customData  = JsonSerializer.Deserialize<JsonElement>("{}");
+
+        public string _allDirectionsEnvironmentName = "N/A";
+        public string _environmentName = "N/A";
+        public string _coverImageFilename = "N/A";
+        public string _songFilename = "N/A";
+        public float _previewDuration = 0.0f;
+        public float _previewStartTime = 0.0f;
+        public float _shufflePeriod = 0.0f;
+        public float _shuffle = 0.0f;
+        public float _songTimeOffset = 0.0f;
+        public string _version = "N/A";
+    }
+
+    public class BeatSaberSongDifficulty
+    {
+        public float Duration = 0.0f;
+        public float Length = 0.0f;
+        public double _noteJumpMovementSpeed = 0.0;
+        public float _noteJumpStartBeatOffset = 0.0f;
+        public int Bombs = 0;
+        public int Notes = 0;
+        public int Obstacles = 0;
+        public string _difficulty = "N/A";
+        public int _difficultyRank = 0;
+        //public JsonElement _customData  = JsonSerializer.Deserialize<JsonElement>("{}");
+
+        public string _beatmapFilename = "N/A";
+    }
+
+    public class BeatSaberSongBeatMapCharacteristic
+    {
+        public string _beatmapCharacteristicName = "N/A";
+        public List<BeatSaberSongDifficulty> _difficultyBeatmaps = new List<BeatSaberSongDifficulty>();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using BeatSaber;
+using Microsoft.WindowsAPICodePack.Shell;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BS_Map_reverser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Path to map (zip): ");
+            try
+            {
+                String zip = Console.ReadLine().Replace("\"", "");
+                if (Directory.Exists("tmp")) Directory.Delete("tmp", true);
+                Directory.CreateDirectory("tmp");
+                String unzippedPath = "tmp\\" + Path.GetFileNameWithoutExtension(zip) + "\\";
+                ZipFile.ExtractToDirectory(zip, "tmp\\" + Path.GetFileNameWithoutExtension(zip));
+                if (!File.Exists(unzippedPath + "info.dat"))
+                {
+                    throw new Exception("Info.dat is non existent");
+                }
+                BeatSaberSong info = JsonSerializer.Deserialize<BeatSaberSong>(File.ReadAllText(unzippedPath + "info.dat"));
+                if (!File.Exists(unzippedPath + info._songFilename))
+                {
+                    throw new Exception("Song file is missing");
+                }
+                File.Move(unzippedPath + info._songFilename, unzippedPath + info._songFilename.Replace(".egg", ".ogg"));
+                ShellFile song = ShellFile.FromFilePath(unzippedPath + info._songFilename.Replace(".egg", ".ogg"));
+                ulong songLength = (ulong)(song.Properties.System.Media.Duration.Value / 10000000); //Convert 100 ns to second
+                File.Move(unzippedPath + info._songFilename.Replace(".egg", ".ogg"), unzippedPath + info._songFilename);
+                Console.WriteLine("Song length: " + songLength + " seconds");
+                float songTimeInBeats = (songLength - info._songTimeOffset / 1000) * (info.BPM / 60);
+                Console.WriteLine("in beats: " + songTimeInBeats);
+                Directory.CreateDirectory("tmp\\new");
+                String newDirPath = "tmp\\new\\";
+                Console.WriteLine("Copying song");
+                foreach (String s in Directory.GetFiles(unzippedPath)) File.Copy(s, newDirPath + Path.GetFileName(s), true);
+                foreach(BeatSaberSongBeatMapCharacteristic c in info.BeatMapCharacteristics)
+                {
+                    Console.WriteLine("Beat Map Characteristic: " + c.BeatMapCharacteristicName);
+                    foreach(BeatSaberSongDifficulty d in c.Difficulties)
+                    {
+                        Console.WriteLine("Converting difficulty: " + d.DifficultyName);
+                        if(!File.Exists(unzippedPath + d._beatmapFilename))
+                        {
+                            Console.WriteLine("Difficulty file doesn't exist. skipping");
+                            continue;
+                        }
+                        DiffFile orgDiff = JsonSerializer.Deserialize<DiffFile>(File.ReadAllText(unzippedPath + d._beatmapFilename));
+                        DiffFile newDiff = new DiffFile();
+                        foreach(Note n in orgDiff._notes)
+                        {
+                            Note reversed = n;
+                            reversed._time = songTimeInBeats - n._time;
+                            newDiff._notes.Add(reversed);
+                        }
+                        Console.WriteLine("Revesed " + newDiff._notes.Count + " Notes");
+                        foreach (Obstacle o in orgDiff._obstacles)
+                        {
+                            Obstacle reversed = o;
+                            reversed._time = songTimeInBeats - o._time;
+                            newDiff._obstacles.Add(reversed);
+                        }
+                        Console.WriteLine("Revesed " + newDiff._obstacles.Count + " Obstacles");
+                        foreach (Event e in orgDiff._events)
+                        {
+                            Event reversed = e;
+                            reversed._time = songTimeInBeats - e._time;
+                            newDiff._events.Add(reversed);
+                        }
+                        Console.WriteLine("Revesed " + newDiff._events.Count + " Events");
+                        Console.WriteLine("Saving reversed " + d._beatmapFilename);
+                        if (File.Exists(newDirPath + d._beatmapFilename)) File.Delete(newDirPath + d._beatmapFilename);
+                        File.WriteAllText(newDirPath + d._beatmapFilename, JsonSerializer.Serialize(newDiff));
+                    }
+                }
+                Console.WriteLine("Reversing Audio");
+                if (File.Exists(newDirPath + info._songFilename)) File.Delete(newDirPath + info._songFilename);
+                ProcessStartInfo i = new ProcessStartInfo();
+                i.FileName = "ffmpeg.exe";
+                i.Arguments = "-i \"" + unzippedPath + info._songFilename + "\" -af areverse \"" + newDirPath + info._songFilename.Replace(".egg", ".ogg") + "\"";
+                Process.Start(i).WaitForExit();
+                if(info._songFilename.EndsWith(".egg")) info._songFilename = info._songFilename.Replace(".egg", ".ogg");
+                info.SongName += "_reversed";
+                Console.WriteLine("Saving altered info.dat");
+                if (File.Exists(newDirPath + "info.dat")) File.Delete(newDirPath + "info.dat");
+                File.WriteAllText(newDirPath + "info.dat", JsonSerializer.Serialize(info));
+                Console.WriteLine("Zipping reversed song");
+
+                if (File.Exists(info.SongName + "_reversed.zip")) File.Delete(info.SongName + "_reversed.zip");
+                ZipFile.CreateFromDirectory(newDirPath, info.SongName + "_reversed.zip");
+                Console.WriteLine("Finished");
+            } catch (Exception e)
+            {
+                Console.WriteLine("Error: \n" + e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
